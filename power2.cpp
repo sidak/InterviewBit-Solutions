0d8202f184072ef3bedd3e6f90a6202a42354499
sidak <spssachdeva21695@gmail.com>
2016-01-15 00:09:16 +0530

    Remove bug while calling custom log function in power2

--- power2.cpp
+++ power2.cpp
@@ -2,10 +2,13 @@ double intlog(double base, double x) {
     return (log(x) / log(base));
 }
 bool Solution::isPower(int A) {
-    int rt = (int)(sqrt(A));
+    
+    if(A==1) return true;
+    
+    int rt = (int)(floor(sqrt(A)));
     for(int i=2; i<=rt; i++){
-        double lg = intlog(A, i);
-        if(fmod(lg, 1)==0) return true;
+        double lg = intlog(i, A);
+        if(abs(fmod(lg, 1.0) - 0) < 0.000000001) return true;
     }
     
     return false;
