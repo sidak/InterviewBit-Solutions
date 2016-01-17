949a018c2cec82367b9a3604fe453ab1d9e06bb4
sidak <spssachdeva21695@gmail.com>
2016-01-18 00:11:01 +0530

    Add solution for problem reach

--- reach.cpp
+++ reach.cpp
@@ -1,5 +1,11 @@
 // Input : X and Y co-ordinates of the points in order. 
 // Each point is represented by (X[i], Y[i])
 int Solution::coverPoints(vector<int> &X, vector<int> &Y) {
-    
+    int sz = X.size();
+
+    int ans =0;
+    for(int i=1; i<sz; i++){
+        ans += max((int)(abs(X[i]-X[i-1])), (int)(abs(Y[i]-Y[i-1])) );
+    }
+    return ans;
 }
