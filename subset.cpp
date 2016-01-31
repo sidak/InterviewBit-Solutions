d2226c37af65675fa4ea81be29c47916947ea2d9
sidak <spssachdeva21695@gmail.com>
2016-02-01 00:04:22 +0530

    Update solution of problem subset

--- subset.cpp
+++ subset.cpp
@@ -1,6 +1,22 @@
+void subsetHelper(int i, vector<int> & subset, vector<vector<int>> & ans, vector<int> & A){
+    if(i==A.size()){
+        ans.push_back(subset);
+        return;
+    }
+    subsetHelper(i+1, subset, ans, A);
+    subset.push_back(A[i]);
+    subsetHelper(i+1, subset, ans, A);
+    subset.pop_back();
+}
 vector<vector<int> > Solution::subsets(vector<int> &A) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    vector< vector<int> > ans;
+    vector<int> subset;
+    sort(A.begin(), A.end());
+    subsetHelper(0, subset, ans, A);
+    sort(ans.begin(), ans.end());
+    return ans;
 }
